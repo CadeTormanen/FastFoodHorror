0ec91b2d6f64f018c579b406dbc52b5b84313890
aurora-hawkins <157427272+aurora-hawkins@users.noreply.github.com>
2024-03-22 00:54:45 -0700

    Create Node.cs

---
+++ Assets/Code/Scripts/Interactions/Node.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+	public string playerResponse;
+	public string fullResponse;
+	public string dialougePrompt;
+	public LinkedList<Node> pointers;
+	public string printResponse()
+	{
+		//fetch string to put on button
+	}
+	public string printPrompt()
+	{
+		//fetch string array for dialouge prompt
+	}
+}
