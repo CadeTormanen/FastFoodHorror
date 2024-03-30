edb4b46b3839f0d89d8e008176cc812106cf0297
aurora-hawkins <157427272+aurora-hawkins@users.noreply.github.com>
2024-03-29 22:18:00 -0700

    Update Node.cs

--- Assets/Code/Scripts/Interactions/Node.cs
+++ Assets/Code/Scripts/Interactions/Node.cs
@@ -8,12 +8,42 @@ public class Node
 	public string fullResponse;
 	public string dialougePrompt;
 	public LinkedList<Node> pointers;
-	public string printResponse()
+
+	public Node FetchNode(int branchNum)
 	{
-		//fetch string to put on button
+		Node current=null;
+		if(pointers == null)return null;
+		//if (branchNum == 1) current=this.pointers.Find(leaf1);
+		//if (branchNum == 2) current=this.pointers.Find(leaf2);
+		return current;
+	}
+	public Node SetNode(string buttonText, string prompt, string full)
+	{
+		if ((buttonText == null) || (prompt == null) || (full == null))return null;
+		this.playerResponse=buttonText;
+		this.dialougePrompt=prompt;
+		this.fullResponse=full;
+		pointers=null;
+		return this;
+	}
+	public void ConnectNode(Node leaf)
+	{
+		if (pointers == null)
+		{
+			pointers= new LinkedList<Node>();
+		}
+		this.pointers.AddLast(leaf);
+	}
+	public string PrintButtonText()
+	{
+		return playerResponse;
+	}
+	public string PrintResponse()
+	{
+		return fullResponse;
 	}
 	public string printPrompt()
 	{
-		//fetch string array for dialouge prompt
+		return dialougePrompt;
 	}
 }
